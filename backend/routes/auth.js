0df628483e67de323519d804354db6ae4ce60617
nodirbek <nodirbek@mf.uz>
2024-03-14 23:46:49 +0500

    bug fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,13 +11,7 @@ const {
 } = require("../controllers/auth");
 const { isAuthorized, isActiveUser } = require("../middlewares/routeProtect");
 
-router.post(
-  "/email_login",
-  passportLogin,
-  isAuthorized,
-  isActiveUser,
-  email_login
-);
+router.post("/email_login", passportLogin, isActiveUser, email_login);
 router.post("/email_register", passportRegister, email_register);
 router.get("/google", authGoogle);
 router.get("/google/callback", authGoogleRedirect);
